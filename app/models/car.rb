1420c7312814d738cfa3f9377149a6a699534879
ewhitebloom <ewhitebloom@gmail.com>
2014-04-14 09:58:20 -0400

    car unit tests are passing

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,11 @@
 class Car < ActiveRecord::Base
+  validates :color, presence: true
+  validates :year, presence: true
+  validates :mileage, presence: true
+  validates :manufacturer_id, presence: true
+
+  validates_numericality_of :year, greater_than: 1980
+
+  belongs_to :manufacturer
 
 end
